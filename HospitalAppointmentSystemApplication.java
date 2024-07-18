80ca1f73c0517ef9884faa810e8fbe2738f0e282
Avishkar Kathare <109351945+avishkarkathare@users.noreply.github.com>
2024-07-18 11:00:19 +0530

    Add files via upload

---
+++ HospitalAppointmentSystemApplication.java
@@ -0,0 +1,14 @@
+package com.Hospital_Appointment_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAppointmentSystemApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
